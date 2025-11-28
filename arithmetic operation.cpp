1d1de3978db91f8a041fa3a423674c0488c06ca5
56l4gayatri-cmd <56l4gayatri@gmail.com>
2025-11-28 13:51:20 +0530

    Add files via upload

---
+++ arithmetic operation.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	int ch;
+	printf("enter a,b values");
+	scanf("%d%d",&a,&b);
+	printf("menu/n"),
+	printf("1.add,2.subtraction,3.multiplication,4.divide,5.reminder");
+    scanf("%d",&ch);
+    switch ch;
+}
+case identifier 1.c=a+b;
+printf("sum of %d",c);
+break;
+case identifier 2.c=a-b;
+printf("subtraction of %d", c);
+break;
+case identifier 3.c=a*b;
+printf("multiplication of %d",c);
+break;
+case identifer 4.c=a/b;
+printf("division of %d",c);
+break;
+case identifer 5. c=a%b;
+printf("modular of %d",c);
+break;
+default:printf("invalid of option");
+}
+}
